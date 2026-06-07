2a90e28bfd84b580f4a60b6d15d5d77e8ea392db
Shiroah <85611912+Aphathic@users.noreply.github.com>
2026-06-07 17:05:33 +0200

    Initial Commit

---
+++ AIProjectino/AIProjectinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIProjectinoApp.swift
+//  AIProjectino
+//
+//  Created by Crescenzo Di Franco on 07/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct AIProjectinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
